0b8c3b31a63227ae85c854d2180ccacc6bd6d95c
Justin Revenaugh <justinr@umn.edu>
2018-04-27 12:38:39 -0500

    Reformatted for easier readability.

--- global.R
+++ global.R
@@ -1,9 +1,9 @@
 require(tidyverse)
 
-# Impose personal bias on graphics and warnings
+# Establish default ggplot2 theme element and suppress warnings
 theme_set(theme_bw())
+no_x_axis <- theme(axis.line.x = element_blank(),
+                  axis.text.x = element_blank(),
+                  axis.title.x = element_blank(),
+                  axis.ticks.x = element_blank())
 options(warn=-1)
-no_x_axis <- theme( axis.line.x = element_blank(),
-                    axis.text.x = element_blank(),
-                    axis.title.x = element_blank(),
-                    axis.ticks.x = element_blank() )
